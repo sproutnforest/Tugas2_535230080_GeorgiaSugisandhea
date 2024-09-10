a1cea315819b590764dfe68f11d0c2a206be7a9f
briggea <geasandhea19@gmail.com>
2024-09-10 23:42:56 +0700

    CSS update

---
+++ Kalkulator/operations.js
@@ -0,0 +1,12 @@
+function addition(first, second){
+    document.getElementById("res").value = Number(first) + Number(second);
+}
+function substract(first, second){
+    document.getElementById("res").value = Number(first) - Number(second);
+}
+function multiply(first, second){
+    document.getElementById("res").value = Number(first) * Number(second);
+}
+function division(first, second){
+    document.getElementById("res").value = Number(first) / Number(second);
+}
